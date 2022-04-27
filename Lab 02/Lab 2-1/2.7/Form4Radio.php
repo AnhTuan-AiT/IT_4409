71ed7b32e16995ddc99bee93f39b1cf8dc3520d3
tan-nguyendinhhuy <nguyentan65431@gmail.com>
2022-04-27 19:58:16 +0700

    update file report

--- Lab 02/Lab 2-1/2.7/Form4Radio.php
+++ Lab 02/Lab 2-1/2.7/Form4Radio.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Receiving Input</title>
+  <head><title>Receiving Input</title></head>
     <body>
-      <font size="5">Thank You: Got Your Input.</font>
+      <font size=5>Thank You: Got Your Input.</font>
       <?php
         $email=$_POST["email"];
         $contact=$_POST["contact"];
         print ("<br>Your email address is $email"); 
-        print ("<br />Contact preference is $contact"); 
+        print ("<br>Contact preference is $contact"); 
       ?>
     </body>
-  </head>
 </html>
